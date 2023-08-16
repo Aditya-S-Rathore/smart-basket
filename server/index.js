be33b4914cbbcef5a1367dbbd45f36c9e2e1001f
Aditya <20je0062@ese.iitism.ac.in>
2023-08-16 22:26:28 +0530

    add environment var support for port

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 5000
 const mongoDB= require("./db");
 mongoDB();
 
@@ -30,6 +29,9 @@ app.get("*", (req, res) => {
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 }) 
