771f099bdba6e1e1aa60e7714efbf004655ccbee
Aditya <20je0062@ese.iitism.ac.in>
2024-09-26 02:41:30 +0530

    Updated Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import Badge from 'react-bootstrap/Badge';
 import Modal from '../Modal';
 import Cart from '../screens/cart';
 import { useCart } from './ContextReducer';
-import logo from '../logo.jpeg'
+import logo from '../logo.png'
 export default function Navbar() {
 const[cartView,setCartView] =useState(false);
 let data= useCart();
@@ -20,7 +20,9 @@ navigate("/login")
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-info ">
         <div className="container-fluid">
-          <Link  to="/"><img src={logo} style={{width: 120,height:40}} /></Link>
+          <Link  to="/"><img src={logo} style={{ width: 120, height: 60, paddingRight: 20, paddingBottom: 10 }}
+
+ /></Link>
            
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -28,7 +30,7 @@ navigate("/login")
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav me-auto mb-2">
               <li className="nav-item">
-                <Link className="nav-link active fs-5" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link active fs-5" aria-current="page" to="/" >Home</Link>
               </li>
 
               {(localStorage.getItem("authToken")) ?
